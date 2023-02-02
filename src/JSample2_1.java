e25bd3efcad052218901a23a2e27e1530a15f5ee
{dsjanainoyo} <{manouchekatsu@gmail.com}>
2023-02-03 00:08:35 +0900

    practice variable add_2file

---
+++ src/JSample2_1.java
@@ -0,0 +1,14 @@
+
+public class JSample2_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int old, height;
+		old = 20;
+		height = 184;
+		
+		System.out.println("年齢：" + old);
+		System.out.println("身長：" + height);
+	}
+
+}
---
+++ src/JSample4_1.java
@@ -0,0 +1,21 @@
+
+public class JSample4_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int n = 100;
+		long l = 12345678910L;
+		double d = 3.14;
+		float f = 3.14F;
+		char c ='花';
+		boolean b = true;
+		
+		System.out.println("n=" + n);
+		System.out.println("l=" + l);
+		System.out.println("d=" + d);
+		System.out.println("f=" + f);
+		System.out.println("c=" + c);
+		System.out.println("b=" + b);
+	}
+
+}
